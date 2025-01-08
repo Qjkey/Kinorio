1d465c54c3d6f6831c936bcc683879660ac5483f
GroAlexPixel <164654123+Qjkey@users.noreply.github.com>
2025-01-08 14:48:57 +0300

    Update js.js

--- js.js
+++ js.js
@@ -1,1 +1,11 @@
-
+var tg = window.Telegram.WebApp;
+var BackButton = tg.BackButton;
+BackButton.show();
+BackButton.onClick(function() {
+    BackButton.hide();
+    tg.exitFullscreen() 
+    window.location = 'https://qjkey.github.io/Kinorio/';
+});
+tg.onEvent('backButtonClicked', function() {
+    BackButton.hide();
+});
